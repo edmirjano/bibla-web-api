e5dd8499f30bc3abe8c3f822da4036ecb73c0984
Emanuel Gjoni <emanuel.gjoni@eurtostep.it>
2024-02-10 10:50:47 +0100

    create classroom migrations

--- database/migrations/2024_02_10_093107_create_books_table.php
+++ database/migrations/2024_02_10_093107_create_books_table.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('image');
-            $table->rememberToken();
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->text('detailed_info');
+            $table->string('cover')->nullable();
+            $table->string('author');
+            $table->float('rating');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -28,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('books');
     }
 };
--- database/migrations/2024_02_10_093219_create_classrooms_table.php
+++ database/migrations/2024_02_10_093219_create_classrooms_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('classrooms', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('image');
-            $table->rememberToken();
+            $table->string('cover')->nullable();
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('classrooms');
     }
 };
