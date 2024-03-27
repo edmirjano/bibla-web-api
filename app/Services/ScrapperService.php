3d8b762d5b808811dfa3dbc8df06279fa0992392
Emanuel Gjoni <emanuel.gjoni@eurtostep.it>
2024-03-27 20:44:53 +0100

    wip

--- app/Services/ScrapperService.php
+++ app/Services/ScrapperService.php
@@ -11,21 +11,38 @@ use Symfony\Component\HttpClient\HttpClient;
 
 class ScrapperService
 {
-
-
     private array $content;
+    private string $url = "";
+    private int $chapterCount;
+    private int $bookId = 0;
+    private array $bookContent = [];
 
     public function handle()
     {
         $browser = new HttpBrowser(HttpClient::create());
         $urls = $this->readfile();
         $this->content = [];
+
         foreach ($urls as $index => $url) {
-                if($index < 5)
-                $this->scrape($browser, $index, $url);
-                dump($url);
+            $book = Str::between($url, '/7/', '.');
+            $newUrl = explode('.', $book)[0];
+
+            if ($this->url != $newUrl) {
+                $this->bookId++;
+                $this->chapterCount = 0;
+                $this->url=$newUrl;
+            }
+            $this->chapterCount++;
+
+            $bookName = $this->scrape($browser, $index, $url);
+            $this->bookContent[$this->bookId]['Id'] = $this->bookId;
+            $this->bookContent[$this->bookId]['Name'] = $bookName;
+            $this->bookContent[$this->bookId]['Total_Chapters'] = $this->chapterCount;
+
         }
+        $bookData = json_encode($this->bookContent, JSON_PRETTY_PRINT);
         $jsonData = json_encode($this->content, JSON_PRETTY_PRINT);
+        Storage::disk('public')->put('scrapper/book.json', $bookData);
 
         Storage::disk('public')->put('scrapper/chapter.json', $jsonData);
     }
@@ -41,28 +58,19 @@ class ScrapperService
     public function scrape($browser, $index, $url)
     {
         $crawler = $browser->request('GET', $url);
-        $this->content[$index]['Id'] = $index+1;
-        $this->content[$index]['Id_Book'] = 1;
-        $this->content[$index]['Content'] = '';
+        $this->content[$index]['Id'] = $index + 1;
+        $this->content[$index]['Id_Book'] = $this->bookId;
+
         $crawler->filter('h1')->each(function ($node) use ($index) {
             $this->content[$index]['Name'] = $node->html();
-            $this->content[$index]['Slug'] = Str::slug ($node->html());
+            $this->content[$index]['Slug'] = Str::slug($node->html());
         });
+
         $crawler->filter('.ChapterContent_chapter__uvbXo')->each(function ($node) use ( $index) {
-            $this->content[$index]['Content'] = $this->content[$index]['Content'].$node->html();
+            $this->content[$index]['Content'] = $this->content[$index]['Content']??"".$node->html();
         });
         $this->content[$index]['url'] = $url;
-
-    }
-
-
-    //to see edi
-    public function scrapeBooks($browser, $url)
-    {
-        $crawler = $browser->request('GET', $url);
-        $crawler->filterXPath('//*[starts-with(@id, "headlessui-popover-panel-")]')->each(function ($node) {
-            dd($node->html());
-        });
+        return preg_replace('/\d+/', '', $this->content[$index]['Name']);
     }
 
 
