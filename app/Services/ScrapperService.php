bf7f0bc91a440f2db37b8182000892f22886c4fc
Emanuel Gjoni <emanuel.gjoni@eurtostep.it>
2024-03-27 21:05:31 +0100

    Add book file on scrapper

--- app/Services/ScrapperService.php
+++ app/Services/ScrapperService.php
@@ -2,9 +2,8 @@
 
 namespace App\Services;
 
-use Hamcrest\Thingy;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
-
 use Illuminate\Support\Str;
 use Symfony\Component\BrowserKit\HttpBrowser;
 use Symfony\Component\HttpClient\HttpClient;
@@ -20,42 +19,44 @@ class ScrapperService
     public function handle()
     {
         $browser = new HttpBrowser(HttpClient::create());
-        $urls = $this->readfile();
+        $urls = $this->readFile();
         $this->content = [];
 
         foreach ($urls as $index => $url) {
-            $book = Str::between($url, '/7/', '.');
-            $newUrl = explode('.', $book)[0];
+            try {
 
-            if ($this->url != $newUrl) {
-                $this->bookId++;
-                $this->chapterCount = 0;
-                $this->url=$newUrl;
+                $book = Str::between($url, '/7/', '.');
+                $newUrl = explode('.', $book)[0];
+
+                if ($this->url != $newUrl) {
+                    $this->bookId++;
+                    $this->chapterCount = 0;
+                    $this->url = $newUrl;
+                }
+                $this->chapterCount++;
+
+                $bookName = $this->scrape($browser, $index, $url);
+                $this->bookContent[$this->bookId]['Id'] = $this->bookId;
+                $this->bookContent[$this->bookId]['Name'] = $bookName;
+                $this->bookContent[$this->bookId]['Total_Chapters'] = $this->chapterCount;
+
+            } catch (\Exception $exception) {
+                Log::error("Error writing content of url {$url} with message: {$exception->getMessage()}");
             }
-            $this->chapterCount++;
-
-            $bookName = $this->scrape($browser, $index, $url);
-            $this->bookContent[$this->bookId]['Id'] = $this->bookId;
-            $this->bookContent[$this->bookId]['Name'] = $bookName;
-            $this->bookContent[$this->bookId]['Total_Chapters'] = $this->chapterCount;
 
         }
-        $bookData = json_encode($this->bookContent, JSON_PRETTY_PRINT);
-        $jsonData = json_encode($this->content, JSON_PRETTY_PRINT);
-        Storage::disk('public')->put('scrapper/book.json', $bookData);
-
-        Storage::disk('public')->put('scrapper/chapter.json', $jsonData);
+        $this->saveScrapperFiles();
     }
 
 
-    public function readfile()
+    public function readFile()
     {
         $fileContent = Storage::disk('public')->get('scrap.csv');
         $fileContent = explode("\n", $fileContent);
         return array_filter($fileContent);
     }
 
-    public function scrape($browser, $index, $url)
+    public function scrape($browser, $index, $url): array|string|null
     {
         $crawler = $browser->request('GET', $url);
         $this->content[$index]['Id'] = $index + 1;
@@ -66,12 +67,26 @@ class ScrapperService
             $this->content[$index]['Slug'] = Str::slug($node->html());
         });
 
-        $crawler->filter('.ChapterContent_chapter__uvbXo')->each(function ($node) use ( $index) {
-            $this->content[$index]['Content'] = $this->content[$index]['Content']??"".$node->html();
+        $crawler->filter('.ChapterContent_chapter__uvbXo')->each(function ($node) use ($index) {
+            $this->content[$index]['Content'] = $this->content[$index]['Content'] ?? "" . $node->html();
         });
         $this->content[$index]['url'] = $url;
         return preg_replace('/\d+/', '', $this->content[$index]['Name']);
     }
 
+    private function saveScrapperFiles(): void
+    {
+        try {
+            $bookData = json_encode($this->bookContent, JSON_PRETTY_PRINT);
+            $jsonData = json_encode($this->content, JSON_PRETTY_PRINT);
+
+            Storage::disk('public')->put('scrapper/book.json', $bookData);
+            Storage::disk('public')->put('scrapper/chapter.json', $jsonData);
+            Log::info("Success saving Scrapper fils in storage . inside /scrapper");
+
+        } catch (\Exception $exception) {
+            Log::error("Error saving scrapper  files with messages {$exception->getMessage()}");
+        }
+    }
 
 }
