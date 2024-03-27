cfd89dacda039ccdd2a0a958ea4143545dd2336d
Emanuel Gjoni <emanuel.gjoni@eurtostep.it>
2024-03-27 18:52:38 +0100

    fix scrapper

--- app/Console/Commands/RunScrapperCommand.php
+++ app/Console/Commands/RunScrapperCommand.php
@@ -5,14 +5,14 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Services\ScrapperService;
 
-class RunScrappercommand extends Command
+class RunScrapperCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'app:run-scrappercommand';
+    protected $signature = 'run-scrapper-command';
 
     /**
      * The console command description.
--- app/Services/ScrapperService.php
+++ app/Services/ScrapperService.php
@@ -1,49 +1,67 @@
 <?php
 
 namespace App\Services;
+
+use Hamcrest\Thingy;
 use Illuminate\Support\Facades\Storage;
 
+use Illuminate\Support\Str;
 use Symfony\Component\BrowserKit\HttpBrowser;
 use Symfony\Component\HttpClient\HttpClient;
 
-class ScrapperService{
+class ScrapperService
+{
 
 
+    private array $content;
 
     public function handle()
     {
         $browser = new HttpBrowser(HttpClient::create());
         $urls = $this->readfile();
-        foreach ($urls as $index=>$url) {
-            if($index < 1)
-            $this->scrape($browser, $url);
+        $this->content = [];
+        foreach ($urls as $index => $url) {
+                $this->scrape($browser, $index, $url);
         }
+        $jsonData = json_encode($this->content, JSON_PRETTY_PRINT);
+
+        Storage::disk('public')->put('scrapper/chapter.json', $jsonData);
     }
 
 
-    public function readfile(){
-        $fileContent=Storage::disk('public')->get('scrap.csv');
-        $fileContent=explode("\n",$fileContent);
+    public function readfile()
+    {
+        $fileContent = Storage::disk('public')->get('scrap.csv');
+        $fileContent = explode("\n", $fileContent);
         return array_filter($fileContent);
     }
 
-    public function scrape($browser, $url){
+    public function scrape($browser, $index, $url)
+    {
         $crawler = $browser->request('GET', $url);
-//        ChapterContent_reader__Dt27r
-        $crawler->filter('.ChapterContent_chapter__uvbXo')->each(function ($node) {
-            dd($node->html());
+        $this->content[$index]['Id'] = $index+1;
+        $this->content[$index]['Id_Book'] = 1;
+
+        $crawler->filter('h1')->each(function ($node) use ($index) {
+            $this->content[$index]['Name'] = $node->html();
+            $this->content[$index]['Slug'] = Str::slug ($node->html());
         });
+        $crawler->filter('.ChapterContent_content__RrUqA')->each(function ($node) use ( $index) {
+            $this->content[$index]['Content'][] = $node->html();
+        });
+        $this->content[$index]['url'] = $url;
+
     }
 
 
     //to see edi
-    public function scrapeBooks($browser, $url){
+    public function scrapeBooks($browser, $url)
+    {
         $crawler = $browser->request('GET', $url);
         $crawler->filterXPath('//*[starts-with(@id, "headlessui-popover-panel-")]')->each(function ($node) {
             dd($node->html());
         });
     }
-
 
 
 }
