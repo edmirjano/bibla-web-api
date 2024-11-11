9b6e6cb8a01be2bd1a6e175fbe641813ef861aaa
Fiori Caka <fioricaka228@gmail.com>
2024-11-11 13:13:52 +0100

    Updated the Song view to show only important info

--- resources/views/song/index.blade.php
+++ resources/views/song/index.blade.php
@@ -20,35 +20,15 @@
                             <thead>
                                 <tr>
                                     <th
-                                        class="px-5 py-3 border-b-2 border-200-gray bg-light-gray text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                        class="px-5 py-3 border-b-2 border-200-gray bg-light-gray text-left text-s font-semibold text-gray-700 uppercase tracking-wider">
                                         Title
                                     </th>
                                     <th
-                                        class="px-5 py-3 border-b-2 border-200-gray bg-light-gray text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                        class="px-5 py-3 border-b-2 border-200-gray bg-light-gray text-left text-s font-semibold text-gray-700 uppercase tracking-wider">
                                         Author
                                     </th>
                                     <th
-                                        class="px-5 py-3 border-b-2 border-200-gray bg-light-gray text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                                        Release Year
-                                    </th>
-                                    <th
-                                        class="px-5 py-3 border-b-2 border-200-gray bg-light-gray text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                                        Playlist
-                                    </th>
-                                    <th
-                                        class="px-5 py-3 border-b-2 border-200-gray bg-light-gray text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                                        Player
-                                    </th>
-                                    <th
-                                        class="px-5 py-3 border-b-2 border-200-gray bg-light-gray text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                                        Links
-                                    </th>
-                                    <th
-                                        class="px-5 py-3 border-b-2 border-200-gray bg-light-gray text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                                        Lyrics
-                                    </th>
-                                    <th
-                                        class="px-5 py-3 border-b-2 border-200-gray bg-light-gray text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                        class="px-5 py-3 border-b-2 border-200-gray bg-light-gray text-left text-s font-semibold text-gray-700 uppercase tracking-wider">
                                         Action
                                     </th>
                                 </tr>
@@ -69,69 +49,12 @@
                                             @endif
                                         </td>
                                         <td class="px-5 py-5 bg-white text-sm whitespace-nowrap">
-                                            {{ $song->release_year }}
-                                        </td>
-                                        <td class="px-5 py-5 bg-white text-sm whitespace-nowrap">
-                                            @if ($song->playlists->isNotEmpty())
-                                                <ul>
-                                                    @foreach ($song->playlists as $playlist)
-                                                        <li>{{ $playlist->title }}</li>
-                                                    @endforeach
-                                                </ul>
-                                            @endif
-                                        </td>
-                                        <td class="px-5 py-5 bg-white text-sm whitespace-nowrap">
-                                            <audio id="audioPlayer-{{ $song->id }}">
-                                                <source src="{{ asset($song->mp3link) }}" type="audio/mpeg">
-                                                Your browser does not support the audio element.
-                                            </audio>
-                                            <button id="playButton-{{ $song->id }}"
-                                                onclick="togglePlayStop({{ $song->id }})"
-                                                class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                                                Play
-                                            </button>
-                                        </td>
-                                        <td class="px-5 py-5 bg-white text-sm whitespace-nowrap">
-                                            <div>
-                                                Play on
-                                                <a href="{{ $song->yt_link }}" class="hover:underline" target="_blank">
-                                                    Youtube
-                                                </a>/
-                                                <a href="{{ $song->spotify_link }}" class="hover:underline" target="_blank">
-                                                    Spotify
-                                                </a>
-                                            </div>
-                                        </td>
-                                        <td class="px-5 py-5 bg-white text-sm whitespace-nowrap">
-
-                                            <x-add-button id="addCategoryBtn_{{ $song->id }}" class="bg-transparent"
-                                                onclick="showModal('addCategoryModal_{{ $song->id }}')">
-                                                {{ 'Show Lyrics' }}
-                                            </x-add-button>
-
-                                            <div id="addCategoryModal_{{ $song->id }}"
-                                                class="modal hidden fixed inset-0 flex justify-center items-center z-50">
-                                                <div
-                                                    class="modal-content p-6 w-1/2 h-1/2 rounded-lg shadow-xl bg-light-gray relative">
-                                                    <button onclick="hideModal('addCategoryModal_{{ $song->id }}')"
-                                                        class="absolute top-0 right-1 text-3xl text-gray-700 hover:text-gray-900">
-                                                        &times;
-                                                    </button>
-                                                    <textarea
-                                                        class="w-full h-full p-2 text-base border rounded-md resize-none"
-                                                        readonly>
-                                                        {{ $song->lyrics }}
-                                                    </textarea>
-                                                </div>
-                                            </div>
-                                        </td>
-
-                                        <td class="px-5 py-5 bg-white text-sm whitespace-nowrap">
                                             <div class="flex space-x-3 align-middle">
                                                 <a href="{{ route('song.edit', $song->id) }}" class="inline-block">
                                                     <img src="{{ asset('icons/edit.svg') }}" alt="Edit">
                                                 </a>
-                                                <form action="{{ route('song.destroy', $song->id) }}" method="POST">
+                                                <form action="{{ route('song.destroy', $song->id) }}" method="POST"
+                                                    onsubmit="return confirmDelete(event);">
                                                     @csrf
                                                     @method('DELETE')
                                                     <button type="submit" class="self-center">
@@ -174,6 +97,9 @@
 
         function hideModal(modalId) {
             document.getElementById(modalId).classList.add('hidden');
+        }
+        function confirmDelete(event) {
+            return confirm('Are you sure you want to delete this song?');
         }
 
         $(document).ready(function () {
