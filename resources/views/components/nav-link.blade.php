f789f79dcfa903131ca269dbe8144835e8e8a5c7
FioriCaka <fioricaka228@gmail.com>
2024-12-07 12:27:37 +0100

    Updated the links to show better on highlight

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,13 +1,14 @@
 @props(['active', 'iconPath' => null])
 
 @php
-    $classes = ($active ?? false)
-                ? 'inline-flex items-center px-4 py-2  text-xl font-bold leading-7 text-blue-600 focus:outline-none transition duration-150 ease-in-out'
-                : 'inline-flex items-center px-4 py-2  b text-lg font-bold leading-7 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+    $classes =
+        $active ?? false
+            ? 'inline-flex items-center px-4 py-2  text-lg font-bold leading-7 text-blue-600 focus:outline-none transition duration-150 ease-in-out'
+            : 'inline-flex items-center px-4 py-2  b text-lg font-bold leading-7 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
-    @if($iconPath && file_exists(public_path($iconPath)))
+    @if ($iconPath && file_exists(public_path($iconPath)))
         <span class="mr-3">
             {!! file_get_contents(public_path($iconPath)) !!}
         </span>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 <nav x-data="{ openBooks: false, openSongs: false }"
-     class="bg-button-white h-screen fixed transition-all duration-300 w-64 flex flex-col justify-between">
+    class="bg-button-white h-screen fixed transition-all duration-300 w-64 flex flex-col justify-between">
     <!-- Primary Navigation Menu -->
     <div class="px-4 sm:px-6 lg:px-8">
         <div class="flex flex-col">
@@ -12,78 +12,73 @@
 
             <!-- Navigation Links -->
             <div class="space-y-8" id="dashboard-text">
-                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')"
-                            iconPath="icons/dashboard.svg">
+                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" iconPath="icons/dashboard.svg">
                     {{ __('Dashboard') }}
                 </x-nav-link>
             </div>
 
             @role(['admin', 'teacher'])
-            <!-- Books Section -->
-            <div id="books-text">
-                <button @click="openBooks = !openBooks" class="flex items-center">
-                    <x-nav-link :active="request()->routeIs('book.index')" iconPath="icons/books.svg">
-                        {{ __('Study plans') }}
-                    </x-nav-link>
-                    <svg x-bind:class="{ 'rotate-180': openBooks }"
-                         class="w-4 h-4 transform transition-transform duration-300" fill="none"
-                         stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                    </svg>
-                </button>
-                <div x-show="openBooks" class="pl-4 space-y-2">
-                    <x-nav-link :href="route('classroom.index')" :active="request()->routeIs('classroom.index')"
-                                iconPath="icons/classroom.svg">
-                        {{ __('Group') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('category.index')" :active="request()->routeIs('category.index')"
-                                iconPath="icons/category.svg">
-                        {{ __('Category') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('book.index')" :active="request()->routeIs('book.index')"
-                                iconPath="icons/books.svg">
-                        {{ __('Study plans') }}
-                    </x-nav-link>
+                <!-- Books Section -->
+                <div id="books-text">
+                    <button @click="openBooks = !openBooks" class="flex items-center">
+                        <x-nav-link :active="request()->routeIs('classroom.index', 'category.index', 'book.index')
+                            ? 'text-blue-600'
+                            : ''" iconPath="icons/books.svg">
+                            {{ __('Study plans') }}
+                        </x-nav-link>
+                        <svg x-bind:class="{ 'rotate-180': openBooks }"
+                            class="w-4 h-4 transform transition-transform duration-300" fill="none" stroke="currentColor"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                        </svg>
+                    </button>
+                    <div x-show="openBooks" class="pl-4 space-y-2">
+                        <x-nav-link :href="route('classroom.index')" :active="request()->routeIs('classroom.index')" iconPath="icons/classroom.svg">
+                            {{ __('Group') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('category.index')" :active="request()->routeIs('category.index')" iconPath="icons/category.svg">
+                            {{ __('Category') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('book.index')" :active="request()->routeIs('book.index')" iconPath="icons/books.svg">
+                            {{ __('Study plans') }}
+                        </x-nav-link>
+                    </div>
                 </div>
-            </div>
 
-            <!-- Songs Section -->
-            <div id="songs-text">
-                <button @click="openSongs = !openSongs" class="flex items-center w-full">
-                    <x-nav-link :active="request()->routeIs('song.index')" iconPath="icons/songs.svg">
-                        {{ __('Songs') }}
-                    </x-nav-link>
-                    <svg x-bind:class="{ 'rotate-180': openSongs }"
-                         class="w-4 h-4 transform transition-transform duration-300" fill="none"
-                         stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                    </svg>
-                </button>
-                <div x-show="openSongs" class="pl-4 space-y-2">
-                    <x-nav-link :href="route('song.index')" :active="request()->routeIs('song.index')"
-                                iconPath="icons/songs.svg">
-                        {{ __('Songs') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('author.index')" :active="request()->routeIs('author.index')"
-                                iconPath="icons/artist.svg">
-                        {{ __('Authors') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('playlist.index')" :active="request()->routeIs('playlist.index')"
-                                iconPath="icons/playlist.svg">
-                        {{ __('Playlist') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('album.index')" :active="request()->routeIs('album.index')"
-                                iconPath="icons/album.svg">
-                        {{ __('Album') }}
+                <!-- Songs Section -->
+                <div id="songs-text">
+                    <button @click="openSongs = !openSongs" class="flex items-center w-full">
+                        <x-nav-link :active="request()->routeIs('song.index', 'album.index', 'playlist.index', 'author.index')
+                            ? 'text-blue-600'
+                            : ''" iconPath="icons/songs.svg">
+                            {{ __('Songs') }}
+                        </x-nav-link>
+                        <svg x-bind:class="{ 'rotate-180': openSongs }"
+                            class="w-4 h-4 transform transition-transform duration-300" fill="none" stroke="currentColor"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                        </svg>
+                    </button>
+                    <div x-show="openSongs" class="pl-4 space-y-2">
+                        <x-nav-link :href="route('song.index')" :active="request()->routeIs('song.index')" iconPath="icons/songs.svg">
+                            {{ __('Songs') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('author.index')" :active="request()->routeIs('author.index')" iconPath="icons/artist.svg">
+                            {{ __('Authors') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('playlist.index')" :active="request()->routeIs('playlist.index')" iconPath="icons/playlist.svg">
+                            {{ __('Playlist') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('album.index')" :active="request()->routeIs('album.index')" iconPath="icons/album.svg">
+                            {{ __('Album') }}
+                        </x-nav-link>
+                    </div>
+                </div>
+                <div class="space-y-8" id="users-text">
+                    <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')" iconPath="icons/users.svg">
+                        {{ __('Users') }}
                     </x-nav-link>
                 </div>
-            </div>
-            <div class="space-y-8" id="users-text">
-                <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')"
-                            iconPath="icons/users.svg">
-                    {{ __('Users') }}
-                </x-nav-link>
-            </div>
             @endrole
         </div>
     </div>
@@ -92,11 +87,11 @@
     <div class="px-4 py-4 border-t border-gray-200 bg-gray-50" id="end-of-nav">
         <!-- Profile Button -->
         <x-dropdown-link :href="route('profile.edit')"
-                         class="flex items-center space-x-4 text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200 rounded-lg px-4 py-2 transition-colors">
+            class="flex items-center space-x-4 text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200 rounded-lg px-4 py-2 transition-colors">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24"
-                 stroke="currentColor">
+                stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M5.121 12.121l1.415-1.415m0 0l7.778-7.778a2 2 0 112.828 2.828l-7.778 7.778m-3.181.02a2 2 0 102.828 2.828M21 21l-6-6">
+                    d="M5.121 12.121l1.415-1.415m0 0l7.778-7.778a2 2 0 112.828 2.828l-7.778 7.778m-3.181.02a2 2 0 102.828 2.828M21 21l-6-6">
                 </path>
             </svg>
             <span>{{ __('Profile') }}</span>
@@ -106,11 +101,10 @@
         <form method="POST" action="{{ route('logout') }}" class="mt-3">
             @csrf
             <button type="submit"
-                    class="w-full flex items-center space-x-4 text-sm font-medium text-gray-700 bg-gray-100 hover:bg-red-100 hover:text-red-600 rounded-lg px-4 py-2 transition-colors">
+                class="w-full flex items-center space-x-4 text-sm font-medium text-gray-700 bg-gray-100 hover:bg-red-100 hover:text-red-600 rounded-lg px-4 py-2 transition-colors">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24"
-                     stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M17 16l4-4m0 0l-4-4m4 4H7">
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7">
                     </path>
                 </svg>
                 <span>{{ __('Log Out') }}</span>
